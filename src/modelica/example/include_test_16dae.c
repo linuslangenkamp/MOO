aca47abf27861cce11abd15bbd2520d82f828de6
linuslangenkamp <linus.langenkamp@fh-bielefeld.de>
2025-05-14 17:02:09 +0200

    [OPT] rm generated code

--- src/modelica/example/include_test_16dae.c
+++
@@ -1,9 +0,0 @@
-/* DAE residuals is empty */
-#include "include_test_model.h"
-#ifdef __cplusplus
-extern "C" {
-#endif
-int include_test_initializeDAEmodeData(DATA* data, DAEMODE_DATA* daeModeData){ return -1; }
-#ifdef __cplusplus
-}
-#endif
--- src/modelica/example/include_test_16dae.h
+++
@@ -1,4 +0,0 @@
-#ifndef include_test_16DAE_H
-#define include_test_16DAE_H
-#endif
-
