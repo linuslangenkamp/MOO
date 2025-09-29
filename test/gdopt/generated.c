82993feb2d45e6ad38575929daf8c7b082657957
linuslangenkamp <linus.langenkamp@fh-bielefeld.de>
2025-09-29 14:06:44 +0200

    [CTest] Update project structure for CTest 	- move src/runner -> test/gdopt 	- fail tests if not all necessary dependencies are build 	- update Ipopt.cmake: exclude failing test MDistCntrl6a

--- test/gdopt/generated.c
+++ test/gdopt/generated.c
@@ -20,7 +20,7 @@
 
 #include <interfaces/c/structures.h>
 #include <interfaces/gdopt/main_gdopt.h>
-#include <runner/generated.h>
+#include <generated.h>
 
 // === problem sizes (compile const) ===
 
